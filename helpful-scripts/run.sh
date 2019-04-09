18abf090f3c2a87fb70dc612cf50cea619c95273
Cristiano Clemente <32751490+cpcmc@users.noreply.github.com>
2019-04-09 09:43:59 +0100

    Create run.sh

---
+++ helpful-scripts/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python photo_slideshow.py a_example
+python photo_slideshow.py b_lovely_landscapes
+python photo_slideshow.py c_memorable_moments
+python photo_slideshow.py d_pet_pictures
+python photo_slideshow.py e_shiny_selfies
